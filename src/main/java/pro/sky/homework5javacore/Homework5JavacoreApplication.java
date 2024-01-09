2d77d11316972150846cc2bcfb8fbda74acc71c3
mike85k87 <mike85k@yandex.ru>
2024-01-09 13:50:26 +0700

    init

---
+++ src/main/java/pro/sky/homework5javacore/Homework5JavacoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homework5javacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework5JavacoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework5JavacoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/homework5javacore/Homework5JavacoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework5javacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework5JavacoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
